184067fb3b4b93c8e5a0ba122a324034def6e673
David Shapiro <davidashapiro@users.noreply.github.com>
2018-11-18 19:19:40 -0600

    added skype to social links

--- scripts/header_part1.js
+++ scripts/header_part1.js
@@ -21,6 +21,7 @@
 												document.write("<a target='_blank' href='https://twitter.com/DimaShapiro' class='fa fa-twitter' style='color: #ffffff'></a> ");
 												document.write("<a target='_blank' href='https://www.facebook.com/dimavoron' class='fa fa-facebook' style='color: #ffffff'></a> ");
 												document.write("<a href='#' class='fa fa-google' style='color: #ffffff'></a> ");
+												document.write("<a href='skype:dima.shapiro' class='fa fa-skype' style='color: #ffffff'></a> ");
 												document.write("<a target='_blank' href='https://www.flickr.com/photos/taradunn/' class='fa fa-flickr' style='color: #ffffff'></a> ");
 												document.write("<a target='_blank' href='https://www.linkedin.com/in/davidashapiro' class='fa fa-linkedin' style='color: #ffffff'></a> ");
 												document.write("</td>");
