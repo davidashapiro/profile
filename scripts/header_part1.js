1a480d9e4857a86760de1c25cdad3bcc916b30b6
David Shapiro <davidashapiro@users.noreply.github.com>
2018-11-19 19:55:24 -0600

    mofied social icons

--- scripts/header_part1.js
+++ scripts/header_part1.js
@@ -7,6 +7,12 @@
 								document.write(" <ul id='phoneontop'> ");
 									document.write(" <li><i class='fa fa-phone'></i><a href='#'> +1 949-444-2260</a></li> ");
 									document.write(" <li><i class='fa fa-envelope'></i><a href='mailto:d321973@mail.com'> d321973@mail.com</a></li> ");
+									document.write("<li style='float:right;'><a target='_blank' href='https://twitter.com/DimaShapiro' class='fa fa-twitter' style='color: #ffffff'></a></li> ");
+									document.write("<li style='float:right;'><a target='_blank' href='https://www.facebook.com/dimavoron' class='fa fa-facebook' style='color: #ffffff'></a></li> ");
+									document.write("<li style='float:right;'><a href='#' class='fa fa-google' style='color: #ffffff'></a></li> ");
+									document.write("<li style='float:right;'><a href='skype:dima.shapiro' class='fa fa-skype' style='color: #ffffff'></a></li> ");
+									document.write("<li style='float:right;'><a target='_blank' href='https://www.flickr.com/photos/shapirus/' class='fa fa-flickr' style='color: #ffffff'></a></li> ");
+									document.write("<li style='float:right;'><a target='_blank' href='https://www.linkedin.com/in/davidashapiro' class='fa fa-linkedin' style='color: #ffffff'></a></li> ");
 								document.write(" </ul> ");
 								
 								document.write("<table cellpadding='0' cellspacing='0' border='0' width='100%' height='260'>");
@@ -18,12 +24,6 @@
 												document.write("<img src='images/logo.gif' border='0' alt=''><br>");
 												document.write("<div class='mycompany'>David Shapiro</div>");
 												document.write("<div align='center' class='slogan'>Speed, Relibility and Quality</div>");
-												document.write("<a target='_blank' href='https://twitter.com/DimaShapiro' class='fa fa-twitter' style='color: #ffffff'></a> ");
-												document.write("<a target='_blank' href='https://www.facebook.com/dimavoron' class='fa fa-facebook' style='color: #ffffff'></a> ");
-												document.write("<a href='#' class='fa fa-google' style='color: #ffffff'></a> ");
-												document.write("<a href='skype:dima.shapiro' class='fa fa-skype' style='color: #ffffff'></a> ");
-												document.write("<a target='_blank' href='https://www.flickr.com/photos/shapirus/' class='fa fa-flickr' style='color: #ffffff'></a> ");
-												document.write("<a target='_blank' href='https://www.linkedin.com/in/davidashapiro' class='fa fa-linkedin' style='color: #ffffff'></a> ");
 												document.write("</td>");
 												document.write("</tr>");
 											document.write("</table>");
