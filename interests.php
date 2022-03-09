52005156cdd68dfa0bc02d127f7daf7f0a50cb2a
David Shapiro <d321973@gmail.com>
2022-03-09 01:27:06 -0600

    Add files via upload

--- interests.php
+++ interests.php
@@ -26,42 +26,41 @@
 		<span>
 			<div>
 				<div class="images">
-					<img src="icons/png/001-game.png" width="96px" height="96px"/></div>
+					<img src="/images/001-game.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/002-motorbike.png" width="96px" height="96px"/></div>
+					<img src="images/002-motorbike.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/026-sports-and-competition-1.png" width="96px" height="96px"/></div>
+					<img src="images/026-sports-and-competition-1.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/006-beach.png" width="96px" height="96px"/></div>
+					<img src="images/006-beach.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/007-chess.png" width="96px" height="96px"/></div>
+					<img src="images/007-chess.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/009-bowling.png" width="96px" height="96px"/></div>
+					<img src="images/009-bowling.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/014-book.png" width="96px" height="96px"/></div>
+					<img src="images/014-book.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/018-popcorn.png" width="96px" height="96px"/></div>
+					<img src="images/018-popcorn.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/005-scuba-diving.png" width="96px" height="96px"/></div>
+					<img src="images/005-scuba-diving.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/013-meditate.png" width="96px" height="96px"/></div>
+					<img src="images/013-meditate.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/017-weightlifting.png" width="96px" height="96px"/></div>
+					<img src="images/017-weightlifting.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/021-billiard.png" width="96px" height="96px"/></div>
+					<img src="images/021-billiard.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/020-tent.png" width="96px" height="96px"/></div>
+					<img src="images/020-tent.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/023-sports-and-competition.png" width="96px" height="96px"/></div>
+					<img src="images/023-sports-and-competition.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/024-football-table.png" width="96px" height="96px"/></div>
+					<img src="images/024-football-table.png" width="96px" height="96px"/></div>
 				<div class="images">
-					<img src="icons/png/025-ball.png" width="96px" height="96px"/></div>
+					<img src="images/025-ball.png" width="96px" height="96px"/></div>
 			</div>
 		</span>
 		<?php 
-			include 'footer.php';
-			include 'counter.php'; 
+			include 'footer.php'; 
 		?>
 	</body>
 </html>
